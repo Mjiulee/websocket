63b48796bc6c065a17e34c5e451dae10cd3c29b1
mjlee <limj123@189.cn>
2019-05-21 09:39:57 +0800

    U 在k8s默认配置下，nginx转发连不上websocket的问题

--- server_fasthttp.go
+++ server_fasthttp.go
@@ -9,24 +9,11 @@ import (
 	"fmt"
 	"net"
 	"net/url"
-	"sync"
 	"time"
 
-	"github.com/savsgio/gotils"
+	"github.com/valyala/bytebufferpool"
 	"github.com/valyala/fasthttp"
 )
-
-var strPermessageDeflate = []byte("permessage-deflate")
-var strUpgrade = []byte("Upgrade")
-var strWebsocket = []byte("websocket")
-var strWebsocketVersion = []byte("13")
-
-var poolWriteBuffer = sync.Pool{
-	New: func() interface{} {
-		var buf []byte
-		return buf
-	},
-}
 
 // FastHTTPHandler receives a websocket connection after the handshake has been
 // completed. This must be provided.
@@ -38,7 +25,7 @@ type FastHTTPUpgrader struct {
 	// HandshakeTimeout specifies the duration for the handshake to complete.
 	HandshakeTimeout time.Duration
 
-	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes in bytes. If a buffer
+	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes. If a buffer
 	// size is zero, then buffers allocated by the HTTP server are used. The
 	// I/O buffer sizes do not limit the size of the messages that can be sent
 	// or received.
@@ -96,12 +83,15 @@ func (u *FastHTTPUpgrader) responseError(ctx *fasthttp.RequestCtx, status int, r
 }
 
 func (u *FastHTTPUpgrader) selectSubprotocol(ctx *fasthttp.RequestCtx) []byte {
+	value := bytebufferpool.Get()
+	defer bytebufferpool.Put(value)
+
 	if u.Subprotocols != nil {
 		clientProtocols := parseDataHeader(ctx.Request.Header.Peek("Sec-Websocket-Protocol"))
-
 		for _, serverProtocol := range u.Subprotocols {
 			for _, clientProtocol := range clientProtocols {
-				if gotils.B2S(clientProtocol) == serverProtocol {
+				value.SetString(serverProtocol)
+				if bytes.Equal(clientProtocol, value.Bytes()) {
 					return clientProtocol
 				}
 			}
@@ -114,12 +104,16 @@ func (u *FastHTTPUpgrader) selectSubprotocol(ctx *fasthttp.RequestCtx) []byte {
 }
 
 func (u *FastHTTPUpgrader) isCompressionEnable(ctx *fasthttp.RequestCtx) bool {
+	value := bytebufferpool.Get()
+	defer bytebufferpool.Put(value)
+
 	extensions := parseDataHeader(ctx.Request.Header.Peek("Sec-WebSocket-Extensions"))
 
 	// Negotiate PMCE
+	value.SetString("permessage-deflate")
 	if u.EnableCompression {
 		for _, ext := range extensions {
-			if bytes.HasPrefix(ext, strPermessageDeflate) {
+			if bytes.HasPrefix(ext, value.Bytes()) {
 				return true
 			}
 		}
@@ -137,19 +131,28 @@ func (u *FastHTTPUpgrader) isCompressionEnable(ctx *fasthttp.RequestCtx) bool {
 // If the upgrade fails, then Upgrade replies to the client with an HTTP error
 // response.
 func (u *FastHTTPUpgrader) Upgrade(ctx *fasthttp.RequestCtx, handler FastHTTPHandler) error {
+	value := bytebufferpool.Get()
+	defer bytebufferpool.Put(value)
+
 	if !ctx.IsGet() {
 		return u.responseError(ctx, fasthttp.StatusMethodNotAllowed, fmt.Sprintf("%s request method is not GET", badHandshake))
 	}
 
-	if !bytes.Contains(ctx.Request.Header.Peek("Connection"), strUpgrade) {
+	value.SetString("Upgrade")
+	str := "upgrade"
+	if !bytes.Contains(ctx.Request.Header.Peek("Connection"), value.Bytes()) &&
+		!bytes.Contains(ctx.Request.Header.Peek("Connection"), []byte(str)) {
 		return u.responseError(ctx, fasthttp.StatusBadRequest, fmt.Sprintf("%s 'upgrade' token not found in 'Connection' header", badHandshake))
 	}
 
-	if !bytes.Contains(bytes.ToLower(ctx.Request.Header.Peek("Upgrade")), strWebsocket) {
+	value.SetString("websocket")
+	if !bytes.Contains(bytes.ToLower(ctx.Request.Header.Peek("Upgrade")), value.Bytes()) &&
+		!bytes.Contains(bytes.ToLower(ctx.Request.Header.Peek("upgrade")), value.Bytes()) {
 		return u.responseError(ctx, fasthttp.StatusBadRequest, fmt.Sprintf("%s 'websocket' token not found in 'Upgrade' header", badHandshake))
 	}
 
-	if !bytes.Contains(ctx.Request.Header.Peek("Sec-Websocket-Version"), strWebsocketVersion) {
+	value.SetString("13")
+	if !bytes.Contains(ctx.Request.Header.Peek("Sec-Websocket-Version"), value.Bytes()) {
 		return u.responseError(ctx, fasthttp.StatusBadRequest, "websocket: unsupported version: 13 not found in 'Sec-Websocket-Version' header")
 	}
 
@@ -176,6 +179,7 @@ func (u *FastHTTPUpgrader) Upgrade(ctx *fasthttp.RequestCtx, handler FastHTTPHan
 	ctx.SetStatusCode(fasthttp.StatusSwitchingProtocols)
 	ctx.Response.Header.Set("Upgrade", "websocket")
 	ctx.Response.Header.Set("Connection", "Upgrade")
+	ctx.Response.Header.Set("Connection", "upgrade")
 	ctx.Response.Header.Set("Sec-WebSocket-Accept", computeAcceptKeyBytes(challengeKey))
 	if compress {
 		ctx.Response.Header.Set("Sec-WebSocket-Extensions", "permessage-deflate; server_no_context_takeover; client_no_context_takeover")
@@ -186,11 +190,11 @@ func (u *FastHTTPUpgrader) Upgrade(ctx *fasthttp.RequestCtx, handler FastHTTPHan
 
 	ctx.Hijack(func(netConn net.Conn) {
 		// var br *bufio.Reader  // Always nil
-		writeBuf := poolWriteBuffer.Get().([]byte)
+		var writeBuf []byte
 
 		c := newConn(netConn, true, u.ReadBufferSize, u.WriteBufferSize, u.WriteBufferPool, nil, writeBuf)
 		if subprotocol != nil {
-			c.subprotocol = gotils.B2S(subprotocol)
+			c.subprotocol = string(subprotocol)
 		}
 
 		if compress {
@@ -202,9 +206,6 @@ func (u *FastHTTPUpgrader) Upgrade(ctx *fasthttp.RequestCtx, handler FastHTTPHan
 		netConn.SetDeadline(time.Time{})
 
 		handler(c)
-
-		writeBuf = writeBuf[0:0]
-		poolWriteBuffer.Put(writeBuf)
 	})
 
 	return nil
@@ -216,16 +217,16 @@ func fastHTTPcheckSameOrigin(ctx *fasthttp.RequestCtx) bool {
 	if len(origin) == 0 {
 		return true
 	}
-	u, err := url.Parse(gotils.B2S(origin))
+	u, err := url.Parse(string(origin))
 	if err != nil {
 		return false
 	}
-	return equalASCIIFold(u.Host, gotils.B2S(ctx.Host()))
+	return equalASCIIFold(u.Host, string(ctx.Host()))
 }
 
 // FastHTTPIsWebSocketUpgrade returns true if the client requested upgrade to the
 // WebSocket protocol.
 func FastHTTPIsWebSocketUpgrade(ctx *fasthttp.RequestCtx) bool {
-	return bytes.Equal(ctx.Request.Header.Peek("Connection"), strUpgrade) &&
-		bytes.Equal(ctx.Request.Header.Peek("Upgrade"), strWebsocket)
+	return bytes.Equal(ctx.Request.Header.Peek("Connection"), []byte("Upgrade")) &&
+		bytes.Equal(ctx.Request.Header.Peek("Upgrade"), []byte("websocket"))
 }
